f39f08e39ec1c86a4598187a2e88c07b7fa981f7
Maycon Kabitschke <mayconkabitschke@gmail.com>
2025-01-02 19:40:14 -0300

    first commit

---
+++ src/pages/FormStep2/index.tsx
@@ -0,0 +1,9 @@
+import * as C from './style';
+
+export const FormStep2 = () => {
+  return (
+    <C.Container>
+      Passo2
+    </C.Container>
+  )
+}
---
+++ src/pages/FormStep3/index.tsx
@@ -0,0 +1,9 @@
+import * as C from './style';
+
+export const FormStep3 = () => {
+  return (
+    <C.Container>
+      Passo3
+    </C.Container>
+  )
+}
