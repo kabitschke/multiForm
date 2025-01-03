2326a55303a55fa8241fe69037b5842e21eeb6cd
Maycon Kabitschke <mayconkabitschke@gmail.com>
2025-01-03 09:19:27 -0300

    step1 style

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -2,10 +2,27 @@ import { Theme } from '@/components/Theme';
 import * as C from './style';
 
 export const FormStep1 = () => {
+
+  const handleNextStep = () => {
+
+  }
+
   return (
     <Theme>
       <C.Container>
-        Passo1
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <label htmlFor="">Seu nome Completo
+          <input type="text"
+            autoFocus
+          />
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
       </C.Container>
 
     </Theme>
