89d46160613a923c0727c113dadaeffc465eab99
Maycon Kabitschke <mayconkabitschke@gmail.com>
2025-01-03 10:27:44 -0300

    step1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,9 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+import { useForm, FormActions } from '@/contexts/formContext'
 import { Theme } from '@/components/Theme';
 import * as C from './style';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1 = () => {
 
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    })
+  }, []);
+
   const handleNextStep = () => {
+    if (state.name !== '') {
+      navigate('/step2');
+    } else {
+      alert('Preencha os dados.')
+    }
+  }
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value
+
+    });
 
   }
 
@@ -19,6 +45,8 @@ export const FormStep1 = () => {
         <label htmlFor="">Seu nome Completo
           <input type="text"
             autoFocus
+            value={state.name}
+            onChange={handleNameChange}
           />
         </label>
 
